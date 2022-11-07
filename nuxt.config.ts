5d00e9b3671a765d0026c89cd006cad9c0a9a29e
aldrich <jslilin987654321@gmail.com>
2022-11-07 16:03:51 +0800

    feat: remove bin.js and used baseUrl in config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     app: {
+        baseURL: '/stable-diffution-utils-project',
         head: {
             charset: 'utf-16',
             viewport: 'width=500, initial-scale=1',
